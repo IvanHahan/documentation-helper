67c22146c0d17284a5f83a732051a1f853b1b6ab
Ivan Hahanov <ivan.hahanov@stereologic.com>
2024-02-27 13:29:45 +0000

    update ingestion script

--- ingestion.py
+++ ingestion.py
@@ -5,22 +5,23 @@ load_dotenv()
 import os
 
 from langchain_community.document_loaders import ReadTheDocsLoader
-from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Pinecone as PineconeLangChain
-from pinecone import Pinecone
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.embeddings.huggingface_hub import HuggingFaceHubEmbeddings
+from tqdm import tqdm
 
+def batchify(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
 
-pc = Pinecone(
-    api_key=os.environ.get("PINECONE_API_KEY"),
-)
 
 INDEX_NAME = "langchain-doc-index"
 
 
 def ingest_docs():
     loader = ReadTheDocsLoader(
-        "langchain-docs/api.python.langchain.com/en/latest/chains"
+        "langchain-docs/langchain.readthedocs.io/en/latest/modules/chains"
     )
 
     raw_documents = loader.load()
@@ -28,14 +29,24 @@ def ingest_docs():
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
     documents = text_splitter.split_documents(raw_documents)
+    print(f'Splitted into {len(documents)} chunks')
+    
     for doc in documents:
         new_url = doc.metadata["source"]
         new_url = new_url.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    embeddings = HuggingFaceHubEmbeddings(
+        huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    )
     print(f"Going to add {len(documents)} to Pinecone")
-    PineconeLangChain.from_documents(documents, embeddings, index_name=INDEX_NAME)
+    
+    for i, doc_batch in tqdm(enumerate(batchify(documents, 50))):
+        if i == 0:
+            db = FAISS.from_documents(doc_batch, embeddings)
+        else:
+            db.add_documents(doc_batch)
+    db.save_local('langchain-docs')
     print("****Loading to vectorstore done ***")
 
 
